dc14844371f130acb8947e3260153c3a8f48ddfd
Layla <msqscience91@gmail.com>
2019-06-15 18:44:34 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Flow (Snake Game) Pseudocode
+1. Set up the canvas
+2. Set score to zero
+3. Create the snake
+4. Create the Apple
+5. Canvas gets cleard every 100ms (everything needs to be called again every 100 ms Game functions)
+6. Draw score - function
+7. move snake in direction of key - function
+Snake Actions:
+- if collides with itself or the wall (GAME OVER)
+- if snake eats apple (add to score, move apple to random location, make snake longer)
+9. Draw Things
+- snake using for loop - function
+- apple - function
+- boreder - function
+10. Key pressing instructions (left, up, right, down)
+*/
+
